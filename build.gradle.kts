78e45b2c42cc9897a2f5af3fbc2a3f144b65103f
kahverengi <ahmet@duruer.dev>
2026-01-14 13:04:55 +0300

    chore: fixed spring dependency issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ val pf4jVersion: String by project
 val vertxVersion: String by project
 val gsonVersion: String by project
 val handlebarsVersion: String by project
+val springContextVersion: String by project
 val bootstrap = (project.findProperty("bootstrap") as String?)?.toBoolean() ?: false
 val noui = project.hasProperty("noui")
 val pluginsDir: File? by rootProject.extra
@@ -82,6 +83,9 @@ dependencies {
     compileOnly("io.vertx:vertx-json-schema:${vertxVersion}")
     compileOnly("io.vertx:vertx-web-validation:${vertxVersion}")
     compileOnly("io.vertx:vertx-web-client:${vertxVersion}")
+
+    // https://mvnrepository.com/artifact/org.springframework/spring-context
+    compileOnly("org.springframework:spring-context:${springContextVersion}")
 }
 
 tasks {
